eccfad6d1611d17685c7c487b2c913fb26684d51
Rahul Jha <rahuljhads556@gmail.com>
2026-06-17 10:14:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Equal Left and Right Subarray Sum/equal-left-and-right-subarray-sum.java
+++ Difficulty: Easy/Equal Left and Right Subarray Sum/equal-left-and-right-subarray-sum.java
@@ -3,20 +3,18 @@
 class Solution {
     int equalSum(int[] arr) {
         // Write your code here
-        int prefLeft[]=new int[arr.length];
-        int prefRight[]=new int[arr.length];
-        prefLeft[0]=0;
+        int prefix[]=new int[arr.length];
+        int suffix[]=new int[arr.length];
+        prefix[0]=0;
         for(int i=1;i<arr.length;i++){
-            prefLeft[i]=prefLeft[i-1]+arr[i-1];
+            prefix[i]=prefix[i-1]+arr[i-1];
         }
-        prefRight[arr.length-1]=0;
+        suffix[arr.length-1]=0;
         for(int i=arr.length-2;i>=0;i--){
-            prefRight[i]=prefRight[i+1]+arr[i+1];
+            suffix[i]=suffix[i+1]+arr[i+1];
         }
         for(int i=0;i<arr.length;i++){
-            if(prefLeft[i]==prefRight[i]){
-                return i;
-            }
+            if(prefix[i]==suffix[i]) return i;
         }
         return -1;
     }
