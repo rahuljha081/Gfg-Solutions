ee4e923a025df8fe6a3a2db816dd91a583cab038
Rahul Jha <rahuljhads556@gmail.com>
2026-06-30 20:09:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isCyclic(int V, int[][] edges) {
+        // code here
+        Queue<Integer> q=new LinkedList<>();
+        int indegree[]=new int[V];
+        for(int i=0;i<edges.length;i++){
+            int to=edges[i][1];
+            indegree[to]++;
+        }
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int from=edges[i][0];
+            int to=edges[i][1];
+            adj.get(from).add(to);
+        }
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int val=q.poll();
+            for(int i=0;i<adj.get(val).size();i++){
+                indegree[adj.get(val).get(i)]--;
+                if(indegree[adj.get(val).get(i)]==0){
+                    q.add(adj.get(val).get(i));
+                }
+            }
+        }
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]!=0) return true;
+        }
+        return false;
+    }
+}
