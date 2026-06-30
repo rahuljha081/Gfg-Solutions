4b8a9bfa4074468eb9912b1ae9c5cad600960eb1
Rahul Jha <rahuljhads556@gmail.com>
2026-06-30 20:37:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule II/course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public ArrayList<Integer> findOrder(int V, int[][] edges) {
+        // code here
+        Queue<Integer> q=new LinkedList<>();
+        int indegree[]=new int[V];
+        for(int i=0;i<edges.length;i++){
+            int from=edges[i][0];
+            indegree[from]++;
+        }
+        ArrayList<Integer> res=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int from=edges[i][0];
+            int to=edges[i][1];
+            adj.get(to).add(from);
+        }
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int val=q.poll();
+            res.add(val);
+            for(int i=0;i<adj.get(val).size();i++){
+                indegree[adj.get(val).get(i)]--;
+                if(indegree[adj.get(val).get(i)]==0){
+                    q.add(adj.get(val).get(i));
+                }
+            }
+        }
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]!=0){
+                return new ArrayList<>();
+            }
+        }
+        return res;
+    }
+}
