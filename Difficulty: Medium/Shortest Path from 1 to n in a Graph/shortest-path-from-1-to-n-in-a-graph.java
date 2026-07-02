6a852e4fd70344f44d29359625a93312982751fb
Rahul Jha <rahuljhads556@gmail.com>
2026-07-02 12:32:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path from 1 to n in a Graph/shortest-path-from-1-to-n-in-a-graph.java
+++ Difficulty: Medium/Shortest Path from 1 to n in a Graph/shortest-path-from-1-to-n-in-a-graph.java
@@ -1,55 +1,60 @@
 class Solution {
     class Pair{
         int node;
-        int wt;
-        Pair(int node,int wt){
+        int distance;
+        Pair(int node,int distance){
             this.node=node;
-            this.wt=wt;
+            this.distance=distance;
         }
     }
     public List<Integer> shortestPath(int n, int m, int edges[][]) {
         //  Code Here.
-        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
-        for(int i=0;i<=n;i++) adj.add(new ArrayList<>());
-        for(int i=0;i<m;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            int w = edges[i][2];
-            adj.get(u).add(new Pair(v, w));
-            adj.get(v).add(new Pair(u, w));
+        List<Integer> res=new ArrayList<>();
+        int dist[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            dist[i]=Integer.MAX_VALUE;
         }
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[1] = 0; 
-        int[] parent = new int[n + 1];
-        for (int i = 1; i <= n; i++) parent[i] = i;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.wt-b.wt);
+        dist[1]=0;
+        int parent[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int from=edges[i][0];
+            int to=edges[i][1];
+            int dis=edges[i][2];
+            adj.get(from).add(new Pair(to,dis));
+            adj.get(to).add(new Pair(from,dis));
+        }
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.distance-b.distance);
         pq.add(new Pair(1,0));
         while(!pq.isEmpty()){
-            Pair curr=pq.poll();
-            int node = curr.node;
-            int currDist = curr.wt;
-            if (currDist > dist[node]) continue;
-            for (int i = 0; i < adj.get(node).size(); i++) {
-                int neigh = adj.get(node).get(i).node;
-                int wtt = adj.get(node).get(i).wt;
-                if (dist[neigh] > dist[node] + wtt) {
-                    dist[neigh] = dist[node] + wtt;
-                    parent[neigh] = node;
-                    pq.add(new Pair(neigh, dist[neigh]));
+            Pair p=pq.poll();
+            int curr_node=p.node;
+            int curr_dist=p.distance;
+            for(int i=0;i<adj.get(curr_node).size();i++){
+                int new_node=adj.get(curr_node).get(i).node;
+                int new_dist=adj.get(curr_node).get(i).distance+dist[curr_node];
+                if(dist[new_node]>new_dist){
+                    parent[new_node]=curr_node;
+                    dist[new_node]=new_dist;
+                    pq.add(new Pair(new_node,new_dist));
                 }
             }
         }
-         if (dist[n] == Integer.MAX_VALUE) return Arrays.asList(-1);
-        List<Integer> path = new ArrayList<>();
-        int node = n;
-        while(parent[node]!=node){
-            path.add(node);
-            node=parent[node];
+        if(dist[n]==Integer.MAX_VALUE) return Arrays.asList(-1);
+        int j=n;
+        while(parent[j]!=j){
+            res.add(j);
+            j=parent[j];
         }
-        path.add(1);
-        Collections.reverse(path);
-        path.add(0,dist[n]);
-        return path;
+        res.add(1);
+        Collections.reverse(res);
+        res.add(0,dist[n]);
+        return res;
     }
 }
